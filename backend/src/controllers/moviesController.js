9946c59175d94ebb26395b552552f4a5652b909d
sunn02 <sollopez0002@hotmail.com>
2025-09-13 18:07:30 -0300

    feat(controller): add searching and get from external api function

--- backend/src/controllers/moviesController.js
+++ backend/src/controllers/moviesController.js
@@ -1,3 +1,4 @@
+const { fetchMovies, getMovie } = require('../services/omdbService');
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
@@ -8,20 +9,20 @@ exports.getAllMovies = async(req, res) => {
             orderBy: {
                 vote: 'desc',
             },});
-        res.json(result)
+        res.status(200).json(result);
     } catch (error) {
         res.status(500).json({ error: 'Error retrieving data', details: error.message });
     }
 }
 
 exports.createMovie = async(req, res) => {
-    const { movie } = req.body;
+    const newMovie = req.body;
 
     try {
         const result = await prisma.movie.create({
-            data: movie,
+            data: newMovie,
         });
-        res.json(result);
+        res.status(201).json(result);
     } catch (error){
         res.status(500).json({ error: 'Error creating data', details: error.message });
     }
@@ -34,7 +35,8 @@ exports.deleteMovie = async(req, res) => {
         await prisma.movie.delete({
             where: { id: Number(id) }
         });
-    } catch {
+        res.status(204).send();
+    } catch(error) {
         res.status(500).json({ error: 'Error deleting data', details: error.message });
     }
 };
@@ -46,8 +48,29 @@ exports.voteMovie = async (req, res) => {
             where: { id: Number(id) },
             data: { vote: { increment: 1 } },
         });
-        res.json(result);
+        res.status(200).json(result);
     } catch (error) {
         res.status(500).json({ error: 'Error updating data', details: error.message });
     }
 };
+
+exports.searchMovies = async (req, res) => {
+    try {
+        const { title } = req.query;
+        const result = await fetchMovies(title);
+        res.status(200).json(result);
+        } catch (error) {
+        res.status(500).json({ error: 'Error fetching data', details: error.message });
+    }
+};
+
+exports.getMoviefromApi = async (req, res) => {
+    try {
+        const { imdbID } = req.query;
+        const result = await getMovie(imdbID);
+        res.status(200).json(result);
+        } catch (error) {
+        res.status(500).json({ error: 'Error fetching data', details: error.message });
+    }
+}
+
--- backend/src/routes/moviesRoutes.js
+++ backend/src/routes/moviesRoutes.js
@@ -5,9 +5,11 @@ const Controller = require("../controllers/moviesController");
 
 router.use(methodOverride("_method")); 
 
-router.get("/", Controller.getAllMovies);
-router.post("/", Controller.createMovie);
-router.delete("/:id", Controller.deleteMovie);
+router.get("/movies", Controller.getAllMovies);
+router.post("/movie", Controller.createMovie);
+router.delete("/movie/:id", Controller.deleteMovie);
 router.post('/vote/:id', Controller.voteMovie);
+router.get('/search', Controller.searchMovies);
+router.get('/movie', Controller.getMoviefromApi);
 
 module.exports = router;
