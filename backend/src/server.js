9c67fd65d4ae18b20d4dd16daa1ca4b2d33a8f16
sunn02 <sollopez0002@hotmail.com>
2025-09-13 18:11:23 -0300

    refactor(server): remove unused code and simplify middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const path = require('path');
-const methodOverride = require('method-override');
 const route = require('./routes/moviesRoutes');
+const cors = require('cors');
 const app = express();
 const PORT = 8001;
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+require('dotenv').config();
 
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.use(methodOverride('_method'));
-
-app.use("/", route);
+app.use("/api", route);
 
 app.listen(PORT, console.log(`Server listening on http://localhost:${PORT}`));
